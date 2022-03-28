cab623c6c16a9e1ea90ba82d3de98ba4b984737a
alexisloic21 <englebert.alexis@gmail.com>
2022-03-28 14:05:36 +0200

    shaders: Modified version

--- shaders/default_frag.glsl
+++ shaders/default_frag.glsl
@@ -1,7 +1,7 @@
-#version 100
-
+#version 300 es
+layout(location=0) out mediump  vec4 color;
 //varying mediump vec4 color; // TODO: marche pas à cause de la version :/
 void main(void)
 {
-    gl_FragColor = vec4(1.0,1.0,1.0,1.0);
+    color = vec4(1.0,1.0,0.0,1.0);
 }
