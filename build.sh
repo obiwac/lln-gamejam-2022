2a7906bdf090a5252714d7c2462743ab42d03c5c
obiwac <obiwac@gmail.com>
2022-04-01 16:48:12 +0200

    build: Update to C11 standard

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mkdir -p bin
 
 # not using something "proper" like 'make', because it'll make things simpler (this is a smaller project so it doesn't really matter)
 
-cc -g -std=c99 src/main.c -o bin/out \
+cc -g -std=c11 src/main.c -o bin/out \
 	-I/usr/local/include -L/usr/local/lib \
 	-lm -pthread \
 	-lxcb -lxcb-ewmh -lxcb-icccm -lX11 -lX11-xcb -lEGL
